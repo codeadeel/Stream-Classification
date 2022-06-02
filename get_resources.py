6f6f7333bb28f71b923fade5a4b4548a0770b69a
Adeel Ijaz <87276133+codeadeel@users.noreply.github.com>
2022-06-03 00:51:48 +0500

    Fixed Typos

--- get_resources.py
+++ get_resources.py
@@ -17,7 +17,7 @@ import gdown
 
 dlinks = {
     'ffmpeg': 'https://drive.google.com/file/d/1mZ-Pst32ZWrLKvFi-pki_kSOFsPVSJOm/view?usp=sharing',
-    'youtube_dl' : 'https://drive.google.com/file/d/1y8-9IUxz3s6D9Gw4OjrvhBy7MhRjzOpR/view?usp=sharing',
+    'youtube-dl' : 'https://drive.google.com/file/d/1y8-9IUxz3s6D9Gw4OjrvhBy7MhRjzOpR/view?usp=sharing',
     'convnext_tiny-983f1562.pth' : 'https://drive.google.com/file/d/1oErfEfqfb5llR7JW2qgyVh9d3ZrQ6r6l/view?usp=sharing',
     'convnext.model' : 'https://drive.google.com/file/d/14aAEwiOs6Me1yQ2Ff7xiNsNJz2WM_IQT/view?usp=sharing',
     'OHE.labels' : 'https://drive.google.com/file/d/1CazgVhZEzWaHffX_-FCb7jIHJptDTDHI/view?usp=sharing'
